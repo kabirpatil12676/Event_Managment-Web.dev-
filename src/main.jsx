6c18aa2a0ad7e0200377206ec265bcb316af5853
Rushabh Shiradkar <rushabhshiradkar1717@gmail.com>
2025-04-06 23:40:11 +0530

    Added landing page

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
+import Landing from "./Landing"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Routes>
-
+        <Route path="/" element={<Landing />} />   
+        <Route path="/signup" element={<SignUp />} /> 
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<App />} />
       </Routes>
     </Router>
   </React.StrictMode>
