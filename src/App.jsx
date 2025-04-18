5a27dafbe4caad494edb819db70b92eb773f62e0
Rushabh Shiradkar <rushabhshiradkar1717@gmail.com>
2025-04-18 20:35:21 +0530

    Final changes in Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignupPage from "./components/SignUp";
 import LoginPage from "./components/Login";
-import Dashboard from "./components/Dashboard/Dashboard"; // ✅ Import your unified Dashboard
+import Dashboard from "./components/Dashboard/Dashboard"; // 
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} /> {/* ✅ Add this */}
+        <Route path="/dashboard" element={<Dashboard />} /> 
       </Routes>
     </Router>
   );
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -16,16 +16,15 @@ const Landing = () => {
     <div className="landing-container">
       <div className="card">
         <div className="icon"><img src={logo}/></div>
-        <h2>Welcome to TPO website</h2>
-        <p>SIMPLE DISCRIPTION ABOUT WEBSITE.......</p>
-        <p>CREATE A NEW ACCOUNT OR REGISTER NOW</p>
+        <h2>Welcome to Event Viewer</h2>
+        <p>Stay informed and organized with all your upcoming, ongoing, and past events. Whether you're an admin managing events or a user exploring what's next, everything you need is right here.</p>
         
         <div className="button-group">
           <button className="primary-button" onClick={handleSignUp}>
             Create account
           </button><br/>
           <button className="secondary-button" onClick={handleSignIn}>
-            Login if already registered
+            Login
           </button>
         </div>
       </div>
